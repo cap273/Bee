12b802023e3c2247658b0d8ac2426b4b6dc8c482
cap273 <cap273@cornell.edu>
2014-02-02 16:03:11 -0500

    Added JUnit Test Case

--- a1Bee/src/Bee.java
+++ a1Bee/src/Bee.java
@@ -1,20 +1,20 @@
 /** An instance (i.e. object) maintains information about a bee. */
 public class Bee {
-	private String nickname; // Name of Bee, length > 0
-	private int hatchYear; // year of hatching, >= 2000
-	/* month of hatching, 1..12 where 1 is January 
-	 * and 12 is December
+	private String nickname; // Name of this Bee, length>0
+	private int hatchYear; // year of hatching of this Bee, >=2000
+	/* month of hatching of this Bee,
+	 * 1..12 where 1 is January and 12 is December
 	*/
 	private int hatchMonth;
-	/* gender of bee, where 'M' is male 
+	/* gender of this Bee, where 'M' is male 
 	 * and 'F' is female. If male, bee has no father
 	 */
 	private char gender;
-	/* Name of mother of this bee. 
+	/* Name of mother of this Bee. 
 	 * Null if unknown.
 	 */
 	private Bee mother;
-	/* Name of father of this bee. 
+	/* Name of father of this Bee. 
 	 * Null if unknown.
 	 */
 	private Bee father;
