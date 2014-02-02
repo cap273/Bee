7b259fa0e6da9fa763763ebb95ffd7d75db8bd61
cap273 <cap273@cornell.edu>
2014-02-02 16:59:24 -0500

    Added part A of assignment, tested successfully

--- a1Bee/src/Bee.java
+++ a1Bee/src/Bee.java
@@ -1,22 +1,87 @@
 /** An instance (i.e. object) maintains information about a bee. */
 public class Bee {
-	private String nickname; // Name of this Bee, length>0
-	private int hatchYear; // year of hatching of this Bee, >=2000
+	/* Name of this Bee, length>0 */
+	private String nickname; 
+	
+	/* year of hatching of this Bee, >=2000 */
+	private int hatchYear;
+	
 	/* month of hatching of this Bee,
 	 * 1..12 where 1 is January and 12 is December
 	*/
 	private int hatchMonth;
+	
 	/* gender of this Bee, where 'M' is male 
 	 * and 'F' is female. If male, bee has no father
 	 */
 	private char gender;
+	
 	/* Name of mother of this Bee. 
 	 * Null if unknown.
 	 */
 	private Bee mother;
+	
 	/* Name of father of this Bee. 
 	 * Null if unknown.
 	 */
 	private Bee father;
+	
+	/* Number of children of this Bee.*/
+	private int numChildren;
+	
+	
+	/** Constructor: an instance with nickname n, gender g, 
+	 * birth year y, and birth month m.
+	 * Its parents are unknown, and it has no children.
+	 * Precondition: n has at least 1 character, y>=2000, m is in 1..12,
+	 * and g is 'M' for male or 'F' for female.
+	 */
+    public Bee(String n, char g, int y, int m) {
+        assert n.length()>0; // Length of string n is greater than 0
+        assert y >= 2000;
+        assert 1 <= m && m <= 12;
+        assert 'M' == g || 'F' == g;
+        nickname= n;
+        hatchYear= y;
+        hatchMonth= m;
+        gender= g;
+        
+    }
+    
+    /** Return this Bee's nickname */
+    public String getNickname(){
+    	return nickname;
+    }
+    
+    /** Return the year this Bee hatched from its egg */
+    public int getYear(){
+    	return hatchYear;
+    }
+    
+    /** Return the month this Bee hatched from its egg */
+    public int getMonth(){
+    	return hatchMonth;
+    }
+    
+    /** Return the value of "This Bee is male." */
+    public boolean isMale(){
+    	return 'M' == gender;
+    }
+    
+    /** Return this Bee's mother (null if unknown) */
+    public Bee getMom(){
+    	return mother;
+    }
+    
+    /** Return this Bee's father (null if unknown) */
+    public Bee getDad(){
+    	return father;
+    }
+    
+    /** Return the number of children of this Bee */
+    public int getNumChildren(){
+    	return numChildren;
+    }
+
 
 }
--- a1Bee/src/BeeTester.java
+++ a1Bee/src/BeeTester.java
@@ -2,12 +2,33 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class BeeTester {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFirstConstructor1() {
+		/* Create new instance of Bee */
+		Bee b= new Bee("Johnny Girl", 'F', 2014, 6);
+		
+		// assertEquals statements
+		assertEquals("Johnny Girl",b.getNickname());
+		assertEquals(false,b.isMale());
+		assertEquals(2014,b.getYear());
+		assertEquals(6,b.getMonth());
+		assertEquals(null,b.getMom());
+		assertEquals(null,b.getDad());
+		assertEquals(0,b.getNumChildren());
+		
+		/* Create second instance of Bee, this time male */
+		Bee b2= new Bee("Johnny Boy", 'M', 2013, 4);
+		
+		// assertEquals statements
+		assertEquals("Johnny Boy",b2.getNickname());
+		assertEquals(true,b2.isMale());
+		assertEquals(2013,b2.getYear());
+		assertEquals(4,b2.getMonth());
+		assertEquals(null,b2.getMom());
+		assertEquals(null,b2.getDad());
+		assertEquals(0,b2.getNumChildren());
 	}
 
 }
