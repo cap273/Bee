fc1de74d6dab5d58418f9fdbd344c6c56b597b81
GeorgiaC <Georgia@Georgia-PC>
2014-03-08 14:49:45 -0500

    test 1 2 3

--- a1Bee/src/Bee.java
+++ a1Bee/src/Bee.java
@@ -1,6 +1,7 @@
 /* Checked Javadoc output, everything OK */
 
 // testing for Georgia
+//testing for Carlos
 
 /** An instance (i.e. object) maintains information about a bee. */
 public class Bee {
